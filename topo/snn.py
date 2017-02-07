060813c3d93e46a3bb774d5821b2c00ef9f56200
Prabodh Agarwal <prabodh1194@gmail.com>
2017-02-07 18:31:15 +0530

    Listen to SNMP messages.

---
+++ topo/snn.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = ''
+PORT = 162
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((HOST,PORT))
+
+while 1:
+    data,addr = s.recvfrom(1024)
+    print addr,data
+    if not data: 
+        break
+conn.close()
