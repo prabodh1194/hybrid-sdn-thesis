80ce4ed723892522456915a083e4e3a445ce0a98
Prabodh Agarwal <prabodh1194@gmail.com>
2017-02-26 12:47:46 +0530

    Improved parser to generate time properly

--- topo/parser.py
+++ topo/parser.py
@@ -1,4 +1,4 @@
-import sys, os, pdb, re
+import sys, os, pdb, re, pprint
 
 tcp_file = sys.argv[1]
 
@@ -8,12 +8,18 @@ pattern = "([0-9:.]+) IP ([0-9:.]+) > ([0-9:.]+):"
 
 flag = -1
 
+d = {}
+
 for line in f:
 
     if 'UDP' in line:
         flag = 3
         m = re.search(pattern,line)
-        print m.group(1),m.group(2),m.group(3)
+        t = re.split('[:.]',m.group(1))
+        t = map(float,t)
+
+        s = t[0]*60*60*1000 + t[1]*60*1000 + t[2]*1000 + t[3]/1000.0
+        # print m.group(1),s,m.group(2),m.group(3)
         continue
 
     if flag:
@@ -22,4 +28,11 @@ for line in f:
 
     if flag == 0:
         flag -= 1
-        print line[10:14]
+        # print line[10:14]
+
+        if line[10:14] not in d:
+            d[line[10:14]] = [s,m.group(2),m.group(3)]
+        else:
+            d[line[10:14]] += [s,m.group(2),m.group(3)]
+
+pprint.pprint(d)
