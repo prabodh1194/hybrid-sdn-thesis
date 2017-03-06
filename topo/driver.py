af34f2f32746ab575648e79cb5c0862688840458
Prabodh Agarwal <prabodh1194@gmail.com>
2017-03-06 16:00:03 +0530

    Minor. Start stats collection too

--- topo/driver.py
+++ topo/driver.py
@@ -6,7 +6,7 @@ f = open("res","w",buffering=0)
 
 combo = ['s2', 's2 s4', 's2 s5', 's2 s5 s4', 's3', 's3 s2', 's3 s2 s4', 's3 s2 s5', 's3 s2 s5 s4', 's3 s4', 's3 s5', 's3 s5 s4', 's4', 's5', 's5 s4']
 
-print >> f, os.popen('python tree64-legacy.py -d 3 -f 4').read()
+print >> f, os.popen('python tree64-legacy.py -t -d 3 -f 4').read()
 os.system('cd ../../../stat/ && ls -1 | grep "s[0-9]\+-" | while read var; do sudo tcpdump -qns 0 -X -r $var > s$var; rm $var; done')
 os.system('pwd')
 os.system('python parser.py 3 4 {0} >> ../../../stat/pack_info'.format(','))
@@ -18,7 +18,7 @@ os.system('cd ../../../stat/ && ls -1 | grep "send" | while read var; do sudo rm
 
 for s in combo:
     print s
-    print >> f, os.popen('python tree64-legacy.py -d 3 -f 4 -s {0}'.format(s)).read()
+    print >> f, os.popen('python tree64-legacy.py -t -d 3 -f 4 -s {0}'.format(s)).read()
     os.system('cd ../../../stat/ && ls -1 | grep "s[0-9]\+-" | while read var; do sudo tcpdump -qns 0 -X -r $var > s$var; rm $var; done')
     os.system('pwd')
     os.system('python parser.py 3 4 {0} >> ../../../stat/pack_info'.format(s.replace(' ',',')))
